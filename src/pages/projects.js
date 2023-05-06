ec91da873070a1ce56211eec5552fb56fb29c3c8
Mittow <franco3007199@gmail.com>
2023-05-05 19:48:49 -0500

    implementing responsive on projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -21,19 +21,20 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   return (
     <article
       className="w-full flex items-center justify-between rounded-3xl rounded-br-2xl
-      border border-solid border-dark bg-light shadow-2xl p-12 relative dark:bg-dark dark:border-light"
+      border border-solid border-dark bg-light shadow-2xl p-12 relative dark:bg-dark 
+      dark:border-light lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4"
     >
       {/* ESTE DIV HACE EL EFECTO DE 3D AL CONTENEDOR DE LA IMAGEN */}
       <div
         className="absolute top-0 -right-3 -z-10 w-[100%] h-[103%] rounded-[2.5rem] bg-dark dark:bg-light
-        rounded-br-3xl"
+        rounded-br-3xl xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]"
       />
 
       {/* IMAGEN DEL PROYECTO */}
       <Link
         href={link}
         target="_blank"
-        className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
+        className="w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full"
       >
         <FramerImage 
           src={img} 
@@ -49,9 +50,9 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
       </Link>
 
       {/* INFORMACION DEL PROYECTO */}
-      <div className="w-1/2 flex flex-col items-start justify-between pl-6">
+      <div className="w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6">
         {/* TIPO DE PROYECTO */}
-        <span className="text-primary font-medium text-xl dark:text-primaryDark">{type}</span>
+        <span className="text-primary font-medium text-xl dark:text-primaryDark xs:text-base">{type}</span>
 
         {/* TITULO DEL PROYECTO */}
         <Link
@@ -59,7 +60,7 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
           target="_blank"
           className="hover:underline underline-offset-2"
         >
-          <h2 className="my-2 w-full text-left text-4xl font-bold dark:text-light">{title}</h2>
+          <h2 className="my-2 w-full text-left text-4xl font-bold dark:text-light sm:text-sm">{title}</h2>
         </Link>
 
         {/* RESUMEN DEL PROYECTO */}
@@ -77,7 +78,7 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
             href={link}
             target="_blank"
             className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold
-            dark:bg-light dark:text-dark"
+            dark:bg-light dark:text-dark sm:px-4 sm:text-base"
           >
             Visitar Proyecto
           </Link>
@@ -92,12 +93,13 @@ const Project = ({ title, type, img, link, github }) => {
   return (
     <article
       className="w-full flex flex-col items-center justify-center rounded-2xl border 
-      border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light"
+      border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light
+      xs:p-4"
     >
       {/* ESTE DIV HACE EL EFECTO DE 3D AL CONTENEDOR DE LA IMAGEN */}
       <div
         className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark
-        rounded-br-3xl dark:bg-light"
+        rounded-br-3xl dark:bg-light md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]"
       />
 
       {/* IMAGEN DEL PROYECTO */}
@@ -121,7 +123,11 @@ const Project = ({ title, type, img, link, github }) => {
 
       {/* INFORMACION Y LINKS DEL PROYECTO */}
       <div className="w-full flex flex-col items-start justify-between mt-4">
-        <span className="text-primary font-medium text-xl dark:text-primaryDark">{type}</span>
+        <span 
+          className="text-primary font-medium text-xl dark:text-primaryDark lg:text-lg md:text-base"
+        >
+          {type}
+        </span>
 
         {/* TITULO DEL PROYECTO */}
         <Link
@@ -129,20 +135,24 @@ const Project = ({ title, type, img, link, github }) => {
           target="_blank"
           className="hover:underline underline-offset-2"
         >
-          <h2 className="my-2 w-full text-left text-3xl font-bold">{title}</h2>
+          <h2 
+            className="my-2 w-full text-left text-3xl font-bold lg:text-2xl"
+          >
+            {title}
+          </h2>
         </Link>
         <div className="w-full mt-2 flex items-center justify-between">
           {/* LINK QUE REEDIRIGE A LA PAGINA DEL PROYECTO */}
           <Link
             href={link}
             target="_blank"
-            className="text-lg font-semibold underline"
+            className="text-lg font-semibold underline md:text-base"
           >
             Visit
           </Link>
 
           {/* LINK DEL GITHUB DEL PROYECTO */}
-          <Link href={github} target="_blank" className="w-8">
+          <Link href={github} target="_blank" className="w-8 md:w-6">
             <GithubIcon />
           </Link>
         </div>
@@ -162,11 +172,11 @@ const projects = () => {
         <Layout className="pt-16">
           <AnimatedText
             text="La imaginación trinunfa sobre el conocimiento !"
-            className="mb-16"
+            className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl"
           />
 
           {/* CONTENEDOR DE LOS PROYECTOS */}
-          <div className="grid grip-cols-12 gap-24 gap-y-32">
+          <div className="grid grip-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
 
             {/* PROYECTO 1 (DESTACADO) */}
             <div className="col-span-12">
@@ -182,7 +192,7 @@ const projects = () => {
             </div>
 
             {/* PROYECTO 2 */}
-            <div className="col-span-6">
+            <div className="col-span-6 sm:col-span-12">
               <Project
                 title="E-Commerce"
                 img={project2}
@@ -193,7 +203,7 @@ const projects = () => {
             </div>
 
             {/* PROYECTO 3 */}
-            <div className="col-span-6">
+            <div className="col-span-6 sm:col-span-12">
               <Project
                 title="Pokedux"
                 img={project3}
@@ -217,7 +227,7 @@ const projects = () => {
             </div>
 
             {/* PROYECTO 5 */}
-            <div className="col-span-6">
+            <div className="col-span-6 sm:col-span-12">
               <Project
                 title="PatiChat Frontend"
                 img={project5}
@@ -228,7 +238,7 @@ const projects = () => {
             </div>
 
             {/* PROYECTO 6 */}
-            <div className="col-span-6">
+            <div className="col-span-6 sm:col-span-12">
               <Project
                 title="Patichat Backend"
                 img={project5}
